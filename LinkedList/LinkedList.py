d743b25f1f39550b0b226b33050acd0241b84ba2
Itay <59503091+momohakarish@users.noreply.github.com>
2020-09-04 14:40:14 +0300

    Add files via upload

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -1,3 +1,9 @@
+"""
+A class representing a node in a linked list
+All operations are done in O(1) Space and Time complexity
+"""
+
+
 class Node:
     def __init__(self, value, next_node=None):
         self.value = value
@@ -13,6 +19,11 @@ class Node:
         return self.next_node is not None
 
 
+"""
+A class representing a linked list of nodes
+"""
+
+
 class LinkedList:
     def __init__(self, head: Node = None):
         self.head = head
@@ -20,9 +31,14 @@ class LinkedList:
         if self.head is not None:
             self.size = 1
 
+    # O(1) Time complexity
+    # O(1) Space complexity
     def __len__(self) -> int:
         return self.size
 
+    # O(n) Time complexity
+    # O(n) Space complexity
+    # n = length of the list
     def __str__(self) -> str:
         representation = '['
         iterator = self.head
@@ -31,11 +47,15 @@ class LinkedList:
             iterator = iterator.next_node
         return representation[:-1] + ']'
 
+    # O(1) Time complexity
+    # O(1) Space complexity
     # Adding a node to the list
     def add(self, other: Node):
         other.next_node, self.head = self.head, other
         self.size += 1
 
+    # O(n) Time complexity
+    # O(1) Space complexity
     # Removing a node from the list
     def remove(self, other: Node):
         # In case the node to remove is the first one AKA the head
@@ -49,12 +69,15 @@ class LinkedList:
         while fast_iterator is not None:
             # Removing the node
             if fast_iterator == other:
+                value = fast_iterator.value
                 slow_iterator.next_node = fast_iterator.next_node
             slow_iterator = slow_iterator.next_node
             fast_iterator = fast_iterator.next_node
 
         self.size -= 1
 
+    # O(1) Time complexity
+    # O(1) Space complexity
     # Returning if the list is empty or not
     def is_empty(self) -> bool:
         return self.size == 0
@@ -65,7 +88,6 @@ a = LinkedList()
 a.add(Node(5))
 a.add(Node(23))
 a.add(Node(45))
-a.remove(Node(5))
 print(a)
-print(a.pop())
+a.remove(Node(5))
 print(a)
